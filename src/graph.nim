5a30bd6a079d73a288ed1c0b0568d79ecec1783d
murata <murata@kmc.gr.jp>
2018-06-05 03:17:55 +0900

    invalid data at enum ... (devidegraph?)

--- src/graph.nim
+++ src/graph.nim
@@ -5,17 +5,20 @@ import pietmap, indexto
 # [0,0,rightL] は 分かっているので,いい感じに解析できるはず
 
 type
-  OrderAndSize = tuple[order:Order,size:int]
+  OrderWithInfo = tuple[order:Order,size:int,dp:DP,cc:CC]
   PietProc = tuple[
-    orders:seq[OrderAndSize],
+    orders:seq[OrderWithInfo],
     nexts:seq[int], # stack-top が 0..
     startCC:CC,
     startDP:DP,
   ]
   # 実行命令列と次に起こりうる遷移Index,
 
-proc `$`*(self:OrderAndSize):string =
+proc `$`*(self:OrderWithInfo):string =
   if self.order == Push : return "+{self.size}".fmt
+  if self.order == Pointer : return "DP".fmt
+  if self.order == Switch : return "CC".fmt
+  if self.order == Wall : return "#".fmt
   if self.order == ErrorOrder : return "?{self.size}".fmt
   return $(self.order)
 proc `$`*(self:seq[PietProc]):string =
@@ -46,7 +49,7 @@ proc newGraph(filename:string) : seq[PietProc] =
     proc search(self:var seq[NotDevidedGraph],startCC:CC,startDP:DP,startIndex:int) : int =
       graphIndex += 1
       result = graphIndex # 予め返り値を保存
-      var (cc,dp,index,orders) = (startCC,startDP,startIndex,newSeq[OrderAndSize]())
+      var (cc,dp,index,orders) = (startCC,startDP,startIndex,newSeq[OrderWithInfo]())
       template terminate(nexts :seq[int]= @[]) = self.add(((orders,nexts,startCC,startDP),-1,cc,dp))
       while true:
         if searched[index][cc,dp].graphIndex > 0:
@@ -60,15 +63,15 @@ proc newGraph(filename:string) : seq[PietProc] =
               return pre.graphIndex
             terminate(@[pre.graphIndex])
             return
-          # orders.add((ErrorOrder,pre.orderNum))
           terminate(@[pre.graphIndex])
           self[^1].devideOrderNum = pre.orderNum
           return
         searched[index][cc,dp] = (graphIndex, orders.len())
         let next = indexTo.nextEdges[index][cc,dp]
+        template pushOrder(order:Order=next.order) = orders.add((order,indexTo.blockSize[index],dp,cc))
         case next.order :
         of Switch: # 2方向の可能性に分岐 & 今までの部分をまでをまとめて辺にする
-          orders.add((next.order,indexTo.blockSize[index]))
+          pushOrder()
           terminate()
           let current = self.len() - 1
           for _ in 0..<2:
@@ -76,7 +79,7 @@ proc newGraph(filename:string) : seq[PietProc] =
             cc.toggle()
           return
         of Pointer: # 4方向の可能性に分岐
-          orders.add((next.order,indexTo.blockSize[index]))
+          pushOrder()
           terminate()
           let current = self.len() - 1
           for _ in 0..<4:
@@ -92,18 +95,18 @@ proc newGraph(filename:string) : seq[PietProc] =
               let next = indexTo.nextEdges[index][cc,dp]
               if next.order != Wall: return false
           if isTerminateWall():
+            pushOrder(Terminate)
             terminate()
-            orders.add((Terminate,indexTo.blockSize[index]))
             return
-          orders.add((Wall,indexTo.blockSize[index]))
+          pushOrder()
         of Terminate:
-          orders.add((Terminate,indexTo.blockSize[index]))
+          pushOrder()
           terminate()
           return
         of Nop:
           index = next.index
         else:
-          orders.add((next.order,indexTo.blockSize[index]))
+          pushOrder()
           index = next.index
     # 0-indexに合わせる
     proc to0Indexed(self:var seq[NotDevidedGraph]) =
@@ -163,77 +166,120 @@ proc newGraph(filename:string) : seq[PietProc] =
         let anotherEdge = self[i].pp.nexts[0]
         for j in 0..<self.len():
           self[j].pp.nexts.applyIt(if it == i : anotherEdge else: it)
-    # 長さ0のを削除
-    var deletedSum = 0
-    var deletedSums = newSeq[int](self.len())
-    for i in 0..< self.len():
-      if self[i].pp.orders.len() == 0 :
-        deletedSum += 1
-      deletedSums[i] = deletedSum
-    result = @[]
-    for i in 0..< self.len():
-      if self[i].pp.orders.len() == 0 :continue
-      self[i].pp.nexts.applyIt(it - deletedSums[it])
-      result.add(self[i].pp)
+    return self.mapIt(it.pp)
   # 行かないノードを探して削除
   proc optimizeNext(self:var seq[PietProc]) =
-    for i in 0..<self.len():
-      self[i].nexts = self[i].nexts.deduplicate()
-      let pp = self[i]
-      if pp.nexts.len() <= 1: continue
-      proc testPiet():tuple[suceess:bool,cc:CC,dp:DP] =
-        var dp = pp.startDP
-        var cc = pp.startCC
-        var stack = newStack[int]()
-        proc step(order:Order,size:int) : bool =
-          result = true
-          template binaryIt(op) =
-            if stack.len() < 2 : return false
-            let a {.inject.}= stack.pop()
-            let b {.inject.}= stack.pop()
-            op
-          template unaryIt(op) =
-            if stack.len() < 1 : return false
-            let it {.inject.} = stack.pop()
-            op
-          case order
-          of Add: binaryIt(stack.push(b + a))
-          of Sub: binaryIt(stack.push(b - a))
-          of Mul: binaryIt(stack.push(b * a))
-          of Div: binaryIt(stack.push(b div a))
-          of Mod: binaryIt(stack.push(b mod a))
-          of Greater: binaryIt(stack.push(if b > a : 1 else: 0))
-          of Push: stack.push(size)
-          of Pop: unaryIt((discard))
-          of Not: unaryIt(stack.push(if it > 0 : 0 else: 1))
-          of Pointer: unaryIt(dp.toggle(it))
-          of Switch: unaryIt(if it mod 2 == 1 : cc.toggle())
-          of InC: return false #
-          of InN: return false
-          of OutN: discard
-          of OutC: discard
-          of Dup: unaryIt((stack.push(it);stack.push(it)))
-          of Roll:
-            if stack.len() < 3 : return false
-            let a = stack.pop() # a 回転
-            var b = stack.pop() # 深さ b まで
-            if b > stack.len(): return false
-            var roll = newSeq[int]()
-            for i in 0..<b: roll.add(stack.pop())
-            for i in 0..<b: stack.push(roll[(i + a) mod b])
-          else:discard
-        for order in pp.orders:
-          if not step(order.order,order.size): return (false,cc,dp)
-        return (true,cc,dp)
-      let (success,endCC,endDP) = testPiet()
-      if not success: continue
-      self[i].nexts = self[i].nexts.filterIt(self[it].startCC == endCC and self[it].startDP == endDP)
+    proc execToDetectNeedLessNode(self:var seq[PietProc]) : bool =
+      result = false
+      for i in 0..<self.len():
+        let pp = self[i]
+        if pp.nexts.len() <= 1: continue
+        proc testPiet():tuple[suceess:bool,cc:CC,dp:DP] =
+          var dp = pp.startDP
+          var cc = pp.startCC
+          var stack = newStack[int]()
+          proc step(info:OrderWithInfo) : bool =
+            result = true
+            template binaryIt(op) =
+              if stack.len() < 2 : return false
+              let a {.inject.}= stack.pop()
+              let b {.inject.}= stack.pop()
+              op
+            template unaryIt(op) =
+              if stack.len() < 1 : return false
+              let it {.inject.} = stack.pop()
+              op
+            case info.order
+            of Add: binaryIt(stack.push(b + a))
+            of Sub: binaryIt(stack.push(b - a))
+            of Mul: binaryIt(stack.push(b * a))
+            of Div: binaryIt(stack.push(b div a))
+            of Mod: binaryIt(stack.push(b mod a))
+            of Greater: binaryIt(stack.push(if b > a : 1 else: 0))
+            of Push: stack.push(info.size)
+            of Pop: unaryIt((discard))
+            of Not: unaryIt(stack.push(if it > 0 : 0 else: 1))
+            of Pointer: unaryIt(dp.toggle(it))
+            of Switch: unaryIt(if it mod 2 == 1 : cc.toggle())
+            of InC: return false #
+            of InN: return false
+            of OutN: discard
+            of OutC: discard
+            of Dup: unaryIt((stack.push(it);stack.push(it)))
+            of Roll:
+              if stack.len() < 3 : return false
+              let a = stack.pop() # a 回転
+              var b = stack.pop() # 深さ b まで
+              if b > stack.len(): return false
+              var roll = newSeq[int]()
+              for i in 0..<b: roll.add(stack.pop())
+              for i in 0..<b: stack.push(roll[(i + a) mod b])
+            of Wall:discard
+            else:discard
+            if not(info.order in [Pointer,Switch]):
+              cc = info.cc
+              dp = info.dp
+          for order in pp.orders:
+            if not step(order): return (false,cc,dp)
+          return (true,cc,dp)
+        let (success,endCC,endDP) = testPiet()
+        if not success: continue
+        let newOne = self[i].nexts.filterIt(self[it].startCC == endCC and self[it].startDP == endDP)
+        # ただ一通りに決まる時がある
+        if newOne.len == self[i].nexts.len(): continue
+        result = true
+        self[i].nexts = newOne
+
+    proc deleteNeedLessNode(self:var seq[PietProc]) : bool =
+      proc deleteImpl(self:seq[PietProc]): seq[PietProc] =
+        var isUsed = newSeq[bool](self.len())
+        for pp in self:
+          for next in pp.nexts:
+            isUsed[next] = true
+        var deletedSum = 0
+        var deletedSums = newSeq[int](self.len())
+        for i in 0..< self.len():
+          if not isUsed[i]: deletedSum += 1
+          deletedSums[i] = deletedSum
+        result = @[]
+        for i in 0..< self.len():
+          if not isUsed[i] :continue
+          result.add(self[i])
+          result[^1].nexts.applyIt(it - deletedSums[it])
+      result = false
+      while true:
+        let newOne = self.deleteImpl()
+        if newOne.len == self.len: return
+        self = newOne
+        result = true
+    proc deleteWallNode(self:var seq[PietProc]) =
+      # nexts: stackTop が {None,0} 1 2 3 ... の順と仮定してよい
+      for i in 0..<self.len():
+        if self[i].orders.anyIt(it.order != Wall): continue
+        if self[i].nexts.len != 1 : continue
+        # i へのノードを先っぽに付け替え
+        for j in 0..<self.len():
+          for n in 0..<self[j].nexts.len():
+            if self[j].nexts[n] != i: continue
+            self[j].nexts[n] = self[i].nexts[0]
+      for i in 0..<self.len():
+        if self[i].orders.len == 0 : continue
+        self[i].orders = self[i].orders.filterIt(it.order != Wall)
+
+
+
     # 誰からも参照されない 0 番以外のものを更新されなくなるまで消す
     # 更新されなくなるまで繰り返す
+    while true:
+      discard self.execToDetectNeedLessNode()
+      self.deleteWallNode()
+      if not self.deleteNeedLessNode():break
+
 
 
   let indexTo = filename.newPietMap().newIndexTo()
   var graph = indexTo.makeNotDevidedGraph()
+  # return graph.mapIt(it.pp)
   result = graph.devideGraph()
   result.optimizeNext()
 
@@ -248,8 +294,15 @@ proc makeGraph(self:seq[PietProc]) =
     ];
   """.replace("\n  ","\n")
   for i,pp in self:
-    let content = "{pp.orders}\n{toMinStr(pp.startCC,pp.startDP)}".fmt
-    dot &= fmt"""  a{i} [label = "a{i}\n{content}"];""" & "\n"
+    let order =
+      if pp.orders.len() == 1: $(pp.orders[0])
+      elif pp.orders.len() == 0: ""
+      else: "{pp.orders[0]}..{pp.orders[^1]}".fmt()
+      # else : "{pp.orders}".fmt()
+    let ccdp = "{toMinStr(pp.startCC,pp.startDP)}".fmt
+    var content = "({pp.orders.len()}) {ccdp}\n{order}".fmt
+    if i == 0: content = "START\n" & content
+    dot &= fmt"""  a{i} [label = "{content}"];""" & "\n"
     for next in pp.nexts:
       dot &= fmt"""  a{i} -> a{next} [label = ""];""" & "\n"
   dot &= "}"
