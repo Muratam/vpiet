489b56036011e6712ceda10edd8e4420b17a30bf
murata <murata@kmc.gr.jp>
2018-06-08 22:51:25 +0900

    indexed

--- src/topiet.nim
+++ src/topiet.nim
@@ -1,12 +1,12 @@
 import common
 import nre
 import pietbase
+import tables
 type VPietOrder = enum
   Push,Pop,Add,Sub,Mul,Div,Mod,
   Not,Greater,Dup,Roll,OutN,OutC,InN,InC,
   Terminate,Label,GoIf,Goto,Error
 type OrderAndArgs = tuple[order:VPietOrder,args:seq[string]]
-type LabeledOrders = tuple[name:string,orders:seq[OrderAndArgs]]
 
 proc toOrder(order:seq[string]): OrderAndArgs=
   if order.len == 0 : return (Error,@[])
@@ -48,7 +48,7 @@ proc toOrder(order:seq[string]): OrderAndArgs=
     result.order = GoIf
     return
 
-proc labeling(filename:string): seq[LabeledOrders] =
+proc labeling(filename:string): seq[seq[OrderAndArgs]] =
   let f = open(filename,fmRead)
   defer: f.close()
   let lines = f.readAll().split("\n")
@@ -57,24 +57,37 @@ proc labeling(filename:string): seq[LabeledOrders] =
     if line.len == 0 : continue
     var fun = line.replace(re"#(.*)$","").replace("  ","").split(" ")
     funs.add(fun.toOrder())
-  if funs.len == 0 or funs[0].order != Label : quit("invalid code")
+  if funs.len == 0 or funs[0].order != Label :
+    quit("invalid code")
+  var names = newSeq[string]()
   result = @[]
   for fun in funs:
-    if fun.order == Label: result.add((fun.args[0],@[]))
-    else: result[^1].orders.add(fun)
-proc `$` (self:seq[LabeledOrders]):string =
+    if fun.order == Label:
+      names.add(fun.args[0])
+      result.add(@[])
+    else: result[^1].add(fun)
+  var table = newTable[string,int]()
+  for i,name in names: table[name] = i
+  for i,res in result:
+    for f,fun in res:
+      if not (fun.order in [Goto,GoIf]): continue
+      for j,arg in fun.args:
+        result[i][f].args[j] = $table[arg]
+
+
+proc `$` (self:seq[seq[OrderAndArgs]]):string =
   result = ""
-  for orders in self:
-    result &= orders.name & "\n"
-    for order in orders.orders:
+  for i,orders in self:
+    result &= $i & "\n"
+    for order in orders:
       result &= $order & "\n"
 
-proc toPiet(self:seq[LabeledOrders]) =
+proc toPiet(self:seq[seq[OrderAndArgs]]) =
   var strmap = ""
-  for orders in self:
-    strmap &= orders.name & ":"
+  for i,orders in self:
+    strmap &= $i & ":"
     var nowColor = 0.PietColor
-    for order in orders.orders:
+    for order in orders:
       if order.order == Terminate:
         strmap &= "#"
         continue
