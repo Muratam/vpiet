ab00ce661eff57264fb7496eb088f1e156e25ac2
murata <murata@kmc.gr.jp>
2018-07-22 17:20:12 +0900

    refactor

---
+++ src/pietbase/orderconf.nim
@@ -0,0 +1,23 @@
+import order
+type PietOrderType* = enum
+  NormalOrder,TerminateAtGreater # 命令列の変換は自由
+const pietOrderType = NormalOrder
+
+when pietOrderType == NormalOrder:
+  const orderBlock* = [
+    [ErrorOrder,Push,Pop],
+    [Add,Sub,Mul],
+    [Div,Mod,Not],
+    [Greater,Pointer,Switch],
+    [Dup,Roll,InN],
+    [InC,OutN,OutC],
+  ]
+elif pietOrderType == TerminateAtGreater:
+  const orderBlock* = [
+    [ErrorOrder,Push,Pop],
+    [Add,Sub,Mul],
+    [Div,Mod,Not],
+    [Terminate,Pointer,Switch],
+    [Dup,Roll,InN],
+    [InC,OutN,OutC],
+  ]
