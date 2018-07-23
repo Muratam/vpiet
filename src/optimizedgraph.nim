056f96d3c53d772b8e61a02db3a813c7e7ca8ea1
murata <murata@kmc.gr.jp>
2018-07-23 21:03:31 +0900

    yatteiki

--- src/optimizedgraph.nim
+++ src/optimizedgraph.nim
@@ -1,8 +1,32 @@
 import common
 import pietmap, indexto
 import osproc
+
+type UnionFindTree[T] = ref object
+  data: seq[T]
+  parent: seq[int]
+proc newUnionFindTree(n:int) : UnionFindTree =
+  new(result)
+  result.parent = newSeqWith(n,-1)
+proc root(self:var UnionFindTree,x:int):int =
+  if self.parent[x] < 0 : return x
+  else:
+    self.parent[x] = self.root(self.parent[x])
+    return self.parent[x]
+proc merge(self:var UnionFindTree,x,y:int):bool=
+  var x = self.root(x)
+  var y = self.root(y)
+  if x == y : return false
+  if self.parent[y] < self.parent[x] : (x,y) = (y,x)
+  if self.parent[y] == self.parent[x] : self.parent[x] -= 1
+  self.parent[y] = x
+  return true
+
+
+
 type ShortEdge = tuple[src,dst:int,order:Order,branch:int,size:int]
-  # branch :: dp cc で変更時
+type Edge = tuple[src,dst:int,orderAndSizes:seq[tuple[order:Order,size:int]],branch:int]
+# branch :: dp cc で変更時(realNextとかMerge/DevideNopバグがない)
 proc makeShortEdges(indexTo:IndexTo) : seq[ShortEdge] =
   # block数 x 8 あるのをまず削減
   var nodeIndices = newSeq[EightDirection[int]](indexTo.blockSize.len())
@@ -24,9 +48,8 @@ proc makeShortEdges(indexTo:IndexTo) : seq[ShortEdge] =
           if order != Wall: return
         order = Terminate
       checkWall()
-    # if order == Nop:
-    #   search(nextIndex,dp,cc,currentEdgeIndex,branch)
-    #   return
+    if order == Nop:
+      return search(nextIndex,dp,cc)
     if nodeIndices[index][cc,dp] >= 0:
       return nodeIndices[index][cc,dp]
     maxNodeIndex += 1
@@ -45,7 +68,8 @@ proc makeShortEdges(indexTo:IndexTo) : seq[ShortEdge] =
         to &= (currentNodeIndex,nextNodeIndex,order,i,indexTo.blockSize[index])
         dp.toggle(1)
     of Wall: assert false
-    of Terminate: return currentNodeIndex
+    of Terminate:
+      return currentNodeIndex
     else:
       let nextNodeIndex = search(nextIndex,dp,cc)
       to &= (currentNodeIndex,nextNodeIndex,order,-1,indexTo.blockSize[index])
@@ -55,9 +79,87 @@ proc makeShortEdges(indexTo:IndexTo) : seq[ShortEdge] =
   discard search(0,newDP(),newCC())
   return to
 
+proc toEdges(self:seq[ShortEdge]):seq[Edge] =
+  result = @[]
+  for edge in self:
+    let (src,dst,order,branch,size) = edge
+    result &= (src,dst,@[(order,size)],branch)
+
+proc getMaxNodeIndex(self:seq[Edge]): int =
+  self.mapIt(max(it.src,it.dst)).max() + 1
+
+proc getInEdgesIndexs(self:seq[Edge]) : seq[seq[int]] =
+  result = newSeq[seq[int]](self.getMaxNodeIndex())
+  for i in 0..<result.len(): result[i] = @[]
+  for i,edge in self: result[edge.dst] &= i
+
+proc getOutEdgesIndexs(self:seq[Edge]) : seq[seq[int]] =
+  result = newSeq[seq[int]](self.getMaxNodeIndex())
+  for i in 0..<result.len(): result[i] = @[]
+  for i,edge in self: result[edge.src] &= i
+
+proc deleteNeedlessEdges(self:var seq[Edge]) =
+  while true:
+    var newGraph = newSeq[Edge]()
+    for edge in self:
+      if edge.src >= 0: newGraph &= edge
+    if newGraph.len() == self.len(): return
+    self = newGraph
+    let inEdgesIndexs = self.getInEdgesIndexs()
+    let outEdgesIndexs = self.getOutEdgesIndexs()
+    for i in 0..<self.getMaxNodeIndex():
+      if inEdgesIndexs[i].len() != 0 : continue
+      if outEdgesIndexs[i].len() == 0 : continue
+      if self[outEdgesIndexs[i][0]].src == 0 : continue
+      for oe in outEdgesIndexs[i]:
+        self[oe].src = -1
+
+proc mergeBridge(self:var seq[Edge]) :bool=
+  result = false
+  let inEdgesIndexs = self.getInEdgesIndexs()
+  let outEdgesIndexs = self.getOutEdgesIndexs()
+  for i in 0..<self.getMaxNodeIndex():
+    if inEdgesIndexs[i].len() != 1 : continue
+    if outEdgesIndexs[i].len() != 1 : continue
+    let ie = inEdgesIndexs[i][0]
+    let oe = outEdgesIndexs[i][0]
+    self[oe].src = self[ie].src
+    self[oe].orderAndSizes = self[ie].orderAndSizes & self[oe].orderAndSizes
+    self[ie].src = -1
+    result = true
+  self.deleteNeedlessEdges()
+
+proc filterExitBranch(self:var seq[Edge]) :bool=
+  result = false
+  let inEdgesIndexs = self.getinEdgesIndexs()
+  let outEdgesIndexs = self.getOutEdgesIndexs()
+  for i in 0..<self.getMaxNodeIndex():
+    if inEdgesIndexs[i].len() != 1 : continue
+    if outEdgesIndexs[i].len() < 1 : continue
+    let ie = inEdgesIndexs[i][0]
+    let orders = outEdgesIndexs[i].mapIt(self[it].orderAndSizes[^1].order)
+    let last = self[ie].orderAndSizes[^1]
+    if orders.allIt(it == Pointer):
+      if last.order == Not:
+        for oe in outEdgesIndexs[i]:
+          if self[oe].branch >= 2 :
+            self[oe].src = -1
+            result = true
+      elif last.order == Push:
+        for oe in outEdgesIndexs[i]:
+          if self[oe].branch != last.size mod 4 :
+            self[oe].src = -1
+            result = true
+    elif orders.allIt(it == Switch):
+      if last.order == Push:
+        for oe in outEdgesIndexs[i]:
+          if self[oe].branch != last.size mod 2 :
+            self[oe].src = -1
+            result = true
+  self.deleteNeedlessEdges()
 
 
-proc drawGraph(self:seq[ShortEdge]) : string =
+proc drawGraph(self:seq[Edge]) : string =
   var dot = """digraph pietgraph {
     graph [
       charset = "UTF-8", fontname = "Menlo", style = "filled"
@@ -66,20 +168,26 @@ proc drawGraph(self:seq[ShortEdge]) : string =
       shape = box, fontname = "Menlo", style = "filled",
       fontcolor = "#222222", fillcolor = "#ffffff"
     ];
+    a0 [label=""]
   """.replace("\n  ","\n")
+
   for info in self:
-    let (src,dst,order,branch,size) = info
-    dot &= fmt""" a{src} [label = ""];""" & "\n"
-    dot &= fmt""" a{dst} [label = ""];""" & "\n"
-    let top = if branch < 0 : "" else: $branch
-    let label = "{order}:{top}".fmt()
-    dot &= fmt"""  a{src} -> a{dst} [label = "{label}"];""" & "\n"
+    let (src,dst,orderAndSizes,branch) = info
+    var label = ""
+    for orderAndSize in orderAndSizes:
+      let (order,size) = orderAndSize
+      if order != Push: label &= "{order}\n".fmt()
+      else: label &= "+{size}\n".fmt()
+    let nodeLabel = if src == 0 : "START" else: ""
+    dot &= " a{src} [label = \"{nodeLabel}\"];\n".fmt()
+    let top = if branch < 0 : "" else: ($branch)
+    dot &= fmt"""  a{src} -> a{dst} [label = "{top}:{label}"];""" & "\n"
   dot &= "}"
   return dot
 
 
 
-proc showGraph*(self:seq[ShortEdge],
+proc showGraph*(self:seq[Edge],
                 dotFileName:string="/tmp/piet.dot",
                 pngFileName:string="/tmp/piet.png") =
   let dot = self.drawGraph()
@@ -97,9 +205,15 @@ proc showGraph*(self:seq[ShortEdge],
 # グラフを作成
 proc newGraph*(filename:string) =
   let indexTo = filename.newPietMap().newIndexTo()
-  let to = indexTo.makeShortEdges()
-  echo to
+  var to = indexTo.makeShortEdges().toEdges()
+  while true:
+    echo "check"
+    var updated = false
+    updated = updated or to.mergeBridge()
+    updated = updated or to.filterExitBranch()
+    if not updated: break
   to.showGraph()
+
   # result = indexTo.makeGraph()
 
 
