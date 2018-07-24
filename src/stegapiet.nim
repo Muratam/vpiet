9b28ded3d7fe5976f1bb48d6d3657b4d9271e033
murata <murata@kmc.gr.jp>
2018-07-24 15:42:20 +0900

    color

--- src/stegapiet.nim
+++ src/stegapiet.nim
@@ -1,6 +1,8 @@
 import common
 import pietbase
 import pietize
+import curse
+
 # TODO 分岐なし/Gt=End版を完成させる
 # 1D:
 #   - ビームサーチで Wx1 の画像を作成 (最善であるDPとの比較も可能!!)
@@ -16,17 +18,45 @@ import pietize
 if pietOrderType != TerminateAtGreater:
   quit("only TerminateAtGreater is allowed")
 
+proc `$`*(pietMap:Matrix[PietColor]): string =
+  result = ""
+  for y in 0..<pietMap.height:
+    for x in 0..<pietMap.width:
+      let color = pietMap[x,y]
+      let (r,g,b) = color.toRGB()
+      proc to6(i:uint8):int = (if i == 0xff: 5 elif i == 0xc0: 3 else:1 )
+      let c = case color:
+        of WhiteNumber :
+          getColor6(5,5,5).toBackColor() & getColor6(3,3,3).toForeColor() & '-'
+        of BlackNumber :
+          getColor6(0,0,0).toBackColor() & getColor6(2,2,2).toForeColor() & '*'
+        else:
+          getColor6(r.to6,g.to6,b.to6).toBackColor() & ' '
+      result &=  c
+    result &= "\n"
+  result &= endAll
+
+
+proc `$`(orders:seq[OrderAndArgs]):string =
+  result = ""
+  for order in orders:
+    case order.order :
+    of Operation:
+      if order.operation == Push : result &= "+" & order.args[0]
+      else: result &= $order.operation
+    else: result &= $order.order
+    result &= " "
+
+
+
 proc steagno1D*(orders:seq[OrderAndArgs],base:Matrix[PietColor]) : Matrix[PietColor] =
   # orders : inc dup ... push terminate
   doAssert orders[^1].operation == Terminate         ,"invalid"
   doAssert orders[0..^2].allIt(it.order == Operation),"invalid"
   doAssert base.height == 1
-
-  let maxFunLen = orders.filterIt(not (it.order in[Goto,Goto])).len()
-  let width = maxFunLen + 8 + orders.len() * 2
-  let height = orders.len() * 4 + 1
-  var pietMap = newMatrix[PietColor](width,height)
-  return pietMap
+  result = newMatrix[PietColor](base.width,1)
+  echo orders
+  echo base
 
 proc makeRandomOrders(length:int):seq[OrderAndArgs] =
   randomize()
@@ -54,7 +84,6 @@ proc makeRandomPietColorMatrix*(width,height:int) : Matrix[PietColor] =
 
 if isMainModule:
   let orders = makeRandomOrders(32)
-  echo orders
   let baseImg = makeRandomPietColorMatrix(64,1)
   let stegano = steagno1D(orders,baseImg)
   # baseImg.save()
